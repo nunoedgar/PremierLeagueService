23ec3b45fd1566c021caef8317963fce1c6b9f0b
nunoedgar <edgarpedra99@gmail.com>
2024-09-06 13:45:16 +0100

    DTO's created. HTTP request still return returning models

---
+++ src/main/java/org/nunostudios/premierleagueservice/DTO/PlayerDTO.java
@@ -0,0 +1,40 @@
+package org.nunostudios.premierleagueservice.DTO;
+
+public class PlayerDTO {
+    private Long id;
+    private String name;
+    private Integer age;
+    private Long teamId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+}
--- src/main/java/org/nunostudios/premierleagueservice/DTO/TeamDTO.java
+++ src/main/java/org/nunostudios/premierleagueservice/DTO/TeamDTO.java
@@ -1,4 +1,61 @@
 package org.nunostudios.premierleagueservice.DTO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeamDTO {
+    private Long id;
+    private String name;
+    private String color1;
+    private String color2;
+    private int points;
+    private List<PlayerDTO> players = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor1() {
+        return color1;
+    }
+
+    public void setColor1(String color1) {
+        this.color1 = color1;
+    }
+
+    public String getColor2() {
+        return color2;
+    }
+
+    public void setColor2(String color2) {
+        this.color2 = color2;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public List<PlayerDTO> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<PlayerDTO> players) {
+        this.players = players;
+    }
 }
